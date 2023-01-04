73692c6beb26f4e6f369c65abdd5e76c666cc7f9
mosscantrell <mossc2001@gmail.com>
2023-01-03 16:32:52 -0800

    As a user, when either my partner or I win the game (three squares in a row: horizontally, vertically, or diagonally) I can see a notice telling me which player won.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ const App = () => {
   // create hook for counter
   const [counter, setCounter] = useState(0)
 
+  const [playerX, setPlayerX] = useState([])
+
+  const [playerO, setPlayerO] = useState([])
+
+  const winningCombos = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
+
+  const checkWinner = () => {
+    for (let i=0; i<winningCombos.length; i++) {
+      let counterX = 0
+      let counterO = 0
+      let arr = winningCombos[i]
+      for (let j=0; j<arr.length; j++) {
+        if (playerX.includes(arr[j])){
+          counterX ++ 
+          if (counterX === 3) {
+            alert("Player X wins!")
+          } 
+        }
+      }
+      }
+    }
+    console.log("checkWinner")
+  
+  
   const handleGamePlay = (clickedSquare) => {
     if (squares[clickedSquare] !== null) {
       // console.log(null)
@@ -17,12 +41,20 @@ const App = () => {
       let updateSquare = [...squares]
       updateSquare[clickedSquare] = '❌'
       setSquares(updateSquare)
+      setPlayerX([...playerX, clickedSquare])
     } else {
-    // if counter is odd // ⭕️
+      // if counter is odd // ⭕️
       let updateSquare = [...squares]
       updateSquare[clickedSquare] = '⭕️'
       setSquares(updateSquare)
+      setPlayerO([...playerO, clickedSquare])
     }
+
+    if (counter >= 4) {
+      checkWinner()
+    }
+    
+    console.log(playerX)
 
     let newCounter = counter + 1
 
